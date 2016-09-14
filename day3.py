87bfb1634727aef1fe22113ceb65c6856643f819
destanton <delisestanton@gmail.com>
2016-09-14 12:00:24 -0400

    added lists and dictionary file

--- day3.py
+++ day3.py
@@ -1,3 +1,21 @@
 
-for temp_var in range(5):
+#  for loop for
+for _ in range(5):
     print("i am looping")
+
+turns = 0
+while turns < 5:
+    print("while looping")
+    turns = turns + 1
+    # turns += 1 is the same as above
+
+choice = ""
+while choice != "n":  # way of saying I want to infintely loop through this (think video games)[True]
+    print(choice)
+    choice = input("Do you want to loop again? Y/n ")
+
+#  list collection similar to a string  [] takes any type of input
+#  to find what you want in list you have to use location (index) -- inefficient
+#  dictionary is key, value storage collection
+#  d = {"name": "joel", "age": "33", "fears": "[]"}
+#  d["age"] will return #33 -- more efficient way of searching for something by key (any arbitrary key).
---
+++ day3_collections.py
@@ -0,0 +1,29 @@
+
+my_list = ["danielle", 29, ["falling", "bees"]]
+#  lists always retain the order of input
+print(my_list)
+#  prints order and synntax
+my_list.append("riley")  # add item to the end of a list
+print(my_list)
+my_list.pop()  # when you pop something (from the right) it moves items over (changes index)
+print(my_list)
+print(my_list[1])
+print(my_list[2])
+print(my_list[2][1])  # prints list, index two (which is a list), and then the index of that list.
+
+my_dict = {
+    "name": "danielle",
+    "age": "29",
+    "fears": ["falling", "wasps"]
+}  # there is no perfect data structure. There are times for both lists and dicts.
+
+print(my_dict["name"])
+print(my_dict["age"])
+print(my_dict["fears"])
+
+#  print(dir(my_list))  # dir allows you to see what you can do with the variable passed in.
+#  print(help(my_list))  # help lets you know what you can do to your variable.
+#  navigate help with j and k, or up and down arrow
+#  print(help(my_dict))
+#  print(my_dict.values())
+#  print(my_dict.keys())
