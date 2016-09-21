238829f78ba6e389903badec6f2a52f7985f4547
destanton <delisestanton@gmail.com>
2016-09-21 10:52:49 -0400

    started refactor

--- week2/day3_code_cleanup.py
+++ week2/day3_code_cleanup.py
@@ -3,15 +3,28 @@
 # Inventory
 # Items
 
-player = {}
+player = {"inventory": {"red potion": {"quantity": 20}}}
+{
+    "red potion": {
+        "quantity": 1
+    }
+}
+
 player_name = input("Welcome Traveler! What is your name? ")
 player["name"] = player_name
 print(player)
 
-choice = input("Would you like to (i)nspect your inventory? or (a)dd to your inventory?\n>")
+# want to start from the inside out when improving (refactoring) the code
 
 while True:
+    choice = input("Would you like to (i)nspect your inventory? or (a)dd to your inventory?\n>")
     if choice == 'a':
         print("adding item to inventory")
+        item_name = input("What is the item name? ")
+        item_quantity = int(input("How many? "))
+        player["inventory"][item_name] = {"quantity": item_quantity}
     elif choice == 'i':
         print("looking at inventory")
+        for item in player["inventory"].keys():
+            quantity = player["inventory"][item]["quantity"]
+            print("{} - {}".format(item, quantity))
