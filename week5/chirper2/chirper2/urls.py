e334749fa4a37e4d850181125751b679660d6fba
destanton <delisestanton@gmail.com>
2016-10-14 13:57:46 -0400

    added user/form validation

--- week5/chirper2/chirper2/urls.py
+++ week5/chirper2/chirper2/urls.py
@@ -16,12 +16,13 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 from app.views import index_view, about_view, ChirpView, ChirpDetailView, ChirpCreateView, \
-                       ChirpUpdateView
+                       ChirpUpdateView, UserCreateView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include('django.contrib.auth.urls')),
     url(r'^$', index_view, name="index_view"),
+    url('^create_user/$', UserCreateView.as_view(), name="user_create_view"),
     url(r'^chirps/$', ChirpView.as_view(), name="chirp_view"),
     url(r'^chirps/create/$', ChirpCreateView.as_view(), name="chirp_create_view"),
     url(r'^chirps/(?P<pk>\d+)/$', ChirpDetailView.as_view(), name="chirp_detail_view"),
